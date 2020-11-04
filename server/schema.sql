ce6491d6cb9c21182dfc9225f7a8d3196cf5e3d5
danielharmon <dannyaharmon@gmail.com>
2020-11-03 21:31:44 -0800

    Working on schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,47 @@
-CREATE DATABASE chat;
+/*CREATE DATABASE chat; */
 
 USE chat;
-
-CREATE TABLE messages (
-  /* Describe your table here.*/
+DROP TABLE IF EXISTS `Messages`;
+CREATE TABLE `Messages` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `Message` VARCHAR(300) NOT NULL DEFAULT 'a msg',
+  `User ID` INTEGER NOT NULL DEFAULT 0,
+  `Room ID` INTEGER NOT NULL,
+  `Time Posted` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE `Users` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT  ,
+  `Username` VARCHAR(300) NULL DEFAULT 'username',
+  PRIMARY KEY (`id`)
+);
+DROP TABLE IF EXISTS `Rooms`;
+ CREATE TABLE `Rooms` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `Room Name` VARCHAR(300) NOT NULL DEFAULT 'room name',
+  PRIMARY KEY (`id`)
+
+
+);
+DROP TABLE IF EXISTS `Friends`;
+CREATE TABLE `Friends` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT ,
+  `User ID` INTEGER NOT NULL,
+  `Friend ID` INTEGER NOT NULL,
+  PRIMARY KEY (`id`),
+KEY (`id`)
+);
+
+ALTER TABLE `Messages` ADD CONSTRAINT FOREIGN KEY (User ID) REFERENCES `Users` (`id`);
+-- ALTER TABLE `Messages` ADD FOREIGN KEY (Room ID) REFERENCES `Rooms` (`id`);
+-- ALTER TABLE `Friends` ADD FOREIGN KEY (Friend ID) REFERENCES `Users` (`id`);
+
+
+
+
 /* Create other tables and define schemas for them here! */
-
-
 
 
 /*  Execute this file from the command line by typing:
